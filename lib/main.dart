7fe76ea41a8345b1ff8d239a48fe81716a45b1c2
AnahatM <anahat.mudgal@gmail.com>
2025-02-23 14:46:02 -0500

    Firebase setup

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,13 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:minimalist_social_media/auth/login_or_register.dart';
+import 'package:minimalist_social_media/firebase_options.dart';
 import 'package:minimalist_social_media/themes/dark_mode.dart';
 import 'package:minimalist_social_media/themes/light_mode.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   runApp(const MyApp());
 }
 
